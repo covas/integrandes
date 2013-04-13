395f58ea19ebfc936951f0e3f20c2bd52693044e
covas <cbarraza11@gmail.com>
2013-04-13 07:48:42 -0500

    klklklklklmlkjkl

--- otro/src/otro/otra.java
+++ otro/src/otro/otra.java
@@ -2,6 +2,9 @@ package otro;
 
 public class otra {
 	public static void main(String[] args) {
-		
+		System.out.println( " hello covas");
+		//un cambio
+		//otro cambio
+		System.out.println(" test 2");
 	}
 }
